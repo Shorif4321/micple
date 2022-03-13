d50ecdb2c1a179460ea43ad2a42f0fe4d6ede1ae
Shorif4321 <shorifali4321@gmail.com>
2022-03-13 17:18:35 +0600

    input

--- src/Components/Page/Campaign-information/Campinformation.jsx
+++ src/Components/Page/Campaign-information/Campinformation.jsx
@@ -7,11 +7,11 @@ const Campinformation = () => {
         <div>
             <section className='main-section'>
                 {/* === Left side start */}
-                <div className=''>
+                <div className='left'>
                     <div className='left-side'>
                         <div className='camp-main'>
                             <p className='campaign-info'> Campaign Information</p>
-                            <p className='remove-bookmark'><i class="fas fa-star-of-david"></i>  Remove Bookmarks</p>
+                            <p className='remove-bookmark'><i className="fas fa-star-of-david"></i>  Remove Bookmarks</p>
                         </div>
                         {/* === Image start */}
                         <div>
@@ -75,22 +75,80 @@ const Campinformation = () => {
                     </div>
 
                     <div className='left-side rates-main'>
-
-                        <p className='user-id'>Rates</p>
+                        <p className='user-id rate-type custom-landing'>Rates</p>
                         <div className='rate-head'>
-                        <p >Description</p>
-                        <p >Type</p>
-                        <p >CPA Rate</p>
+                            <p >Description</p>
+                            <p >Type</p>
+                            <p >CPA Rate</p>
                         </div>
                         <div className='rate-head2'>
-                        <p >cc submit</p>
-                        <p className='fixed'>Fixed</p>
-                        <p >$20.00 Per Lead</p>
+                            <p >cc submit</p>
+                            <p className='fixed'>Fixed</p>
+                            <p >$20.00 Per Lead</p>
                         </div>
-
-
                     </div>
 
+                    <div className='left-side rates-main'>
+                        <p className='user-id rate-type custom-landing'>Landing Page</p>
+                        <div className='rate-head '>
+                            <p className=''>Sample</p>
+                            <p >Preview <i className="fas fa-people-carry"></i></p>
+                        </div>
+                        <div className='rate-head '>
+                            <p >Initial (Default)</p>
+                            <p >Preview <i className="fas fa-people-carry"></i></p>
+                        </div>
+                    </div>
+
+                    <div className='left-side rates-main'>
+                        <p className='user-id rate-type custom-landing'>Creatives</p>
+                        <div className='rate-head '>
+                            <p >Text Link</p>
+                            <p >Preview <i className="fas fa-people-carry"></i></p>
+                        </div>
+                        <div className='rate-head '>
+                            <p >Bannar</p>
+                            <p >View</p>
+                        </div>
+                        <div className='rate-head '>
+                            <p >Solo E-mail (text)</p>
+                            <p >View</p>
+                        </div>
+                        <div className='rate-head '>
+                            <p >Solo E-mail (html)</p>
+                            <p >View</p>
+                        </div>
+                    </div>
+
+                    <div className='left-side rates-main custom-bg'>
+                        <p className='user-id rate-type custom-details'>Details</p>
+                        <div className='text-main'>
+                            <div className='text-left'>
+                                <p>Network EPC</p>
+                            </div>
+                            <div className='text-right'>
+                                0.03
+                            </div>
+                        </div>
+
+                        <div className='text-main'>
+                            <div className='text-left'>
+                                <p>Expire Date</p>
+                            </div>
+                            <div className='text-right'>
+                                No expiry
+                            </div>
+                        </div>
+
+                        <div className='text-main'>
+                            <div className='text-left'>
+                                <p>Tracking type</p>
+                            </div>
+                            <div className='text-right'>
+                                Server-to-server tracking
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
 
@@ -98,8 +156,122 @@ const Campinformation = () => {
 
 
                 {/* === right side start */}
-                <div className='right-side'>
-                    <h1>some text will be go here</h1>
+                <div className='right '>
+                    <div className='right-side'>
+                        <div className='camp-desMain'>
+                            <p className='camp-description custom-landing'>Campaign Description</p>
+                            <div className='camp-details'>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam excepturi inventore non ducimus. Aperiam debitis dolore impedit pariatur eius aliquam fugit id accusamus ullam atque qui tempora alias eligendi quis vel rem tenetur vero dolorem distinctio, incidunt inventore, ipsum officiis. Eaque laborum vel sit, nulla commodi voluptatum facere magni quo.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam excepturi inventore non ducimus. Aperiam debitis dolore impedit pariatur eius aliquam fugit id accusamus ullam atque qui tempora alias eligendi quis vel rem tenetur vero dolorem distinctio, incidunt inventore, ipsum officiis. Eaque laborum vel sit, nulla commodi voluptatum facere magni quo.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='res-sear right-side'>
+                        <p className='camp-description custom-landing'>Search Restriction</p>
+                        <div className='camp-details'>
+                            <p className='res-details'>No used of word "free" allowed</p>
+                        </div>
+                    </div>
+
+                    <div className='res-sear right-side '>
+                        <p className='camp-description custom-landing'>Your Tracking link</p>
+                        <div className='camp-details link-generate'>
+                            <div className='link-url '>
+                                <p>https://micple.com/signup https://micple.</p>
+                            </div>
+                            <div className='link-copy'>
+                                <p className='res-details copy-icon'><i className="fas fa-clone"></i> Copy Link </p>
+                            </div>
+                        </div>
+
+                        <div className='traffic-select'>
+                            <h4>1. What type of Traffic do you want to sending</h4>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Contextual</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Display</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Search</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Social</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Native</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Mobile Add</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Email</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Incentive</label>
+                            </div>
+                        </div>
+
+                        <div className='traffic-select'>
+                            <h4>2. Select a Creative</h4>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Row link</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Search </label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Contextual</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Social</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Text link </label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Banners</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Solo E-mail (text)</label>
+                            </div>
+                            <div className='input-add'>
+                                 <input type="radio" />
+                                <label className='' htmlFor="">Solo E-mail (html)</label>
+                            </div>
+                        </div>
+
+                        <div className='traffic-select'>
+                            <h4>3. Select a Creative</h4>
+                            
+                        </div>
+                        
+
+                    </div>
+
+
+
+
+
+
+
+
                 </div>
 
             </section>
